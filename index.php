2d3c1858e4d17d471465a17e14c0922a7a62f652
ninexinnovation <ninexinnovation>
2018-02-21 17:19:32 +0545

    index error fixed

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 
 require_once "Firebase/vendor/autoload.php";
 
+use Kreait\Firebase\Configuration;
+use Kreait\Firebase\Firebase;
+
 $config = new Configuration();
 $config->setAuthConfigFile("webhook-b14b4-firebase-adminsdk-fcj9f-1f5d795eea.json");
 $fb = new Firebase('https://online-khaja-9892c.firebaseio.com/', $config);
 
 $hit = date("Y-m-d hh:mm:ss");
-echo $fb->push($hit, 'hit/');
+$fb->push($hit, 'hit/');
+echo $_GET["hub_challenge"];
 
 ?>
