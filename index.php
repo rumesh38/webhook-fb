7116564b179d785c52c51ae53bcdec793e245890
ninexinnovation <ninexinnovation>
2018-02-21 17:35:36 +0545

    sss

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $config = new Configuration();
 $config->setAuthConfigFile("webhook-b14b4-firebase-adminsdk-fcj9f-1f5d795eea.json");
 $fb = new Firebase('https://webhook-b14b4.firebaseio.com/', $config);
 
-$hit = ["time"=>date("Y-m-d hh:mm:ss")];
+$hit = ["time"=>time()];
 $fb->push($hit, 'hit/');
 echo $_GET["hub_challenge"];
 
