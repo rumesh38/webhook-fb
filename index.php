4c59443711353ac5d2771239888a9cb37f059677
ninexinnovation <ninexinnovation>
2018-02-21 17:21:14 +0545

    array added

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $config = new Configuration();
 $config->setAuthConfigFile("webhook-b14b4-firebase-adminsdk-fcj9f-1f5d795eea.json");
 $fb = new Firebase('https://online-khaja-9892c.firebaseio.com/', $config);
 
-$hit = date("Y-m-d hh:mm:ss");
+$hit = ["time"=>date("Y-m-d hh:mm:ss")];
 $fb->push($hit, 'hit/');
 echo $_GET["hub_challenge"];
 
